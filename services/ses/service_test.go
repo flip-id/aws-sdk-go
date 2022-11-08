3f068f435ba4414d95e943c452ff7c99224cb87f
Pian ZM <pian.zaenal@flip.id>
2022-11-08 11:03:52 +0700

    Add from name as configurable

--- services/ses/service_test.go
+++ services/ses/service_test.go
@@ -4,13 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/flip-id/aws-sdk-go/client"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
 	t.Run("valid new", func(t *testing.T) {
-		client, err := New(context.TODO(), client.APSoutheast1)
+		client, err := New(context.TODO(), &ServiceOption{})
 		assert.NoError(t, err)
 		assert.NotNil(t, client)
 	})
